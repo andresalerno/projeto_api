90559209b2197f1d63b95c80a86bc603e43ed649
andresalerno <andresalerno22@gmail.com>
2024-04-09 22:44:43 -0300

    ajustes docker setup

--- db.sql
+++
@@ -1,9 +0,0 @@
-create database db;
-use db;
-
-create table task (
-id int auto_increment primary key,
-description varchar(255) not null,
-due_date date,
-status ENUM('peding', 'completed') default 'peding'
-);
