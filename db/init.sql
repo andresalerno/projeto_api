f9ed3a905484a24fae370c092a6c2694055b80fb
Eric Lourenco <ericloumendes@gmail.com>
2024-05-21 08:35:58 -0300

    Fixed null graphic generation

--- db/init.sql
+++ db/init.sql
@@ -1,13 +1,23 @@
-CREATE DATABASE knights;
-use knights;
+CREATE DATABASE IF NOT EXISTS CSVRegister;
+USE CSVRegister;
 
-CREATE TABLE favorite_colors (
-  name VARCHAR(20),
-  color VARCHAR(10)
+/* CREATE TABLE IF NOT EXISTS RegistroIndividual (
+id_reg int primary key not null auto_increment,
+dia_reg varchar(7) not null,
+data_reg date not null,
+hora_reg time not null,
+umidade_solo_reg double not null,
+umidade_ambiente_reg double not null,
+temperatura_reg double not null,
+volume_reg double not null
+); */
+
+
+
+CREATE TABLE IF NOT EXISTS Estufa(
+    est_dh datetime not null,
+    est_um_solo double not null,
+    est_um_amb double not null,
+    est_temp double not null,
+    est_vol_aq double not null
 );
-
-INSERT INTO favorite_colors
-  (name, color)
-VALUES
-  ('Lancelot', 'blue'),
-  ('Galahad', 'yellow');
