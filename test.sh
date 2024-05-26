063336bbf3030cd39d529424b1cd7c9eed25ff3c
Eric Lourenço <77221715+ericloumendes@users.noreply.github.com>
2024-05-26 12:10:45 -0300

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,5 +1,15 @@
-echo "HELLO WORLD!"
+echo "Instalador de pacotes do smart-farm"
 
 apt upgrade
 
 echo "Updated apt"
+
+apt install python3 -y
+
+apt install python3-pip -y
+
+echo "Instaled python"
+
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+
+echo "Installed docker"
